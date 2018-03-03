736f4f41572e41ba82ce3bf184c4153bbd213831
misha-sauchuk <36489126+misha-sauchuk@users.noreply.github.com>
2018-03-04 01:47:03 +0300

    Update ex5_00.py

--- HomeWork_05/ex5_00.py
+++ HomeWork_05/ex5_00.py
@@ -1,12 +1,20 @@
 d = {}
 l = [1, 2, 4, 5, 1, 4, 6, 1]
+# Create dict with key-numbers and values is repeated times
 for i in l:
     if i in d:
         d[i] += 1
     else:
-        d[i] = i
+        d[i] = 1
 
-#print(d[1])
+# Create list of key-value pairs
+key_val_list = list(d.items())
 
-for keys in d:
-    print(d[keys])
+# Find the number with max times of repeating
+maximum = None
+for i in key_val_list:
+    if maximum is None:
+        maximum = i[0],i[1]
+    elif maximum[1]<i[1]:
+        maximum = i[0],i[1]
+print('number {} is repeated {} times'.format(maximum[0], maximum[1]))
