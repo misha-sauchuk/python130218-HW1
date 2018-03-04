270b02a0ad99d4debf8cd248cd4564c677d0bd89
misha-sauchuk <36489126+misha-sauchuk@users.noreply.github.com>
2018-03-04 10:50:51 +0300

    Update ex5_03.py

--- HomeWork_05/ex5_03.py
+++ HomeWork_05/ex5_03.py
@@ -9,27 +9,27 @@ list = [
 ]
 
 # Create a dictionary with empty values
-dict_chek = {'name': '', 'surname': '', 'sex': '', 'age': ''}
+dict_check = {'name': '', 'surname': '', 'sex': '', 'age': ''}
 
 # Fucnction to input information to search
 def search_info():
-    for key in dict_chek.keys():
+    for key in dict_check.keys():
         print('Please, enter an information about {}, or press "Enter" to skip:'.format(key.upper()))
-        dict_chek[key] = input()
-    return dict_chek
+        dict_check[key] = input()
+    return dict_check
 
 # Ask function search_info
-dict_chek = search_info()
+dict_check = search_info()
 
 # Count how many parameters we use to search the student
-find_numbres = 0
-for value in dict_chek.values():
-    if value: find_numbres += 1
+find_numbers = 0
+for value in dict_check.values():
+    if value: find_numbers += 1
 
 # Check if there are students in our list with input information
 number = 0
 for dic in list:
-    if dic.items() & dict_chek.items() and len(dic.items() & dict_chek.items())==find_numbres:
+    if dic.items() & dict_check.items() and len(dic.items() & dict_check.items())==find_numbers:
         number += 1
         print('{}. {} {}, {} years old, {}'.format(number, dic['surname'], dic['name'], dic['age'], dic['sex']))
 
